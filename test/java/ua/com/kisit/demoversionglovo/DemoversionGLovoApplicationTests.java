9a428876da1fdafd0a860a7a6c77fb926f5d7bdf
SoftWare <126145175+Andrew1567@users.noreply.github.com>
2026-05-08 17:20:59 +0300

    Add files via upload

---
+++ test/java/ua/com/kisit/demoversionglovo/DemoversionGLovoApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.kisit.demoversionglovo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoversionGLovoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
